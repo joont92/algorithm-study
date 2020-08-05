6fad5d88262ee38fc353ff10cfcd290fd047829e
joont92 <pk920207@gmail.com>
2020-08-06 00:32:46 +0900

    compelete tree circuit....

--- src/main/java/baekjoon/Main.java
+++ src/main/java/baekjoon/Main.java
@@ -1,35 +1,46 @@
 package baekjoon;
 
-import baekjoon.graph.DFS와_BFS;
+import baekjoon.tree.트리_순회;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] condition = br.readLine().split(" ");
-        DFS와_BFS fn = new DFS와_BFS(Integer.parseInt(condition[0]));
 
-        for (int i = 0; i < Integer.parseInt(condition[1]); i++) {
-            String[] relation = br.readLine().split(" ");
-            fn.add(Integer.parseInt(relation[0]), Integer.parseInt(relation[1]));
-        }
-        fn.dfsSearch(Integer.parseInt(condition[2]));
-        System.out.println();
-        fn.bfsSearch(Integer.parseInt(condition[2]));
-    }
-
-    private static void run(BufferedReader br, int count, Consumer<Map<Integer, Integer>> consumer) throws IOException {
-        Map<Integer, Integer> relation = new HashMap<>();
+        int count = Integer.parseInt(br.readLine());
+        트리_순회 fn = new 트리_순회(count);
         for (int i = 0; i < count; i++) {
-            String[] args = br.readLine().split(" ");
-            relation.put(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+            String[] strs = br.readLine().split(" ");
+            fn.add(strs[0].charAt(0), strs[1].charAt(0), strs[2].charAt(0));
         }
-        consumer.accept(relation);
+
+        fn.preOrder('A');
+        System.out.println();
+        fn.inOrder('A');
+        System.out.println();
+        fn.postOrder('A');
+
+//        String[] condition = br.readLine().split(" ");
+//        DFS와_BFS fn = new DFS와_BFS(Integer.parseInt(condition[0]));
+//
+//        for (int i = 0; i < Integer.parseInt(condition[1]); i++) {
+//            String[] relation = br.readLine().split(" ");
+//            fn.add(Integer.parseInt(relation[0]), Integer.parseInt(relation[1]));
+//        }
+//        fn.dfsSearch(Integer.parseInt(condition[2]));
+//        System.out.println();
+//        fn.bfsSearch(Integer.parseInt(condition[2]));
     }
+
+//    private static void run(BufferedReader br, int count, Consumer<Map<Integer, Integer>> consumer) throws IOException {
+//        Map<Integer, Integer> relation = new HashMap<>();
+//        for (int i = 0; i < count; i++) {
+//            String[] args = br.readLine().split(" ");
+//            relation.put(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+//        }
+//        consumer.accept(relation);
+//    }
 }
--- src/main/java/baekjoon/tree/트리_순회.java
+++ src/main/java/baekjoon/tree/트리_순회.java
@@ -2,7 +2,69 @@ package baekjoon.tree;
 
 /**
  * https://www.acmicpc.net/problem/1991
+ *
+ * 전달받은 문자열에서 'A'를 뺴서 배열의 index 에 저장되도록 처리
+ * left, right 에 있는 문자를 전달하고, 문자를 index 바꿔서 다시 배열에서 찾음
+ * 출력하는 순서 조정하여 preOrder, inOrder, postOrder 처리
  */
 public class 트리_순회 {
+    private Node tree[];
 
+    public 트리_순회(int size) {
+        this.tree = new Node[size];
+    }
+
+    public void add(char current, char left, char right) {
+        tree[index(current)] = new Node(current, left, right);
+    }
+
+    public void preOrder(char value) {
+        if(value == '.') {
+            return;
+        }
+
+        Node node = tree[index(value)];
+        System.out.print(node.value);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder(char value) {
+        if(value == '.') {
+            return;
+        }
+
+        Node node = tree[index(value)];
+        inOrder(node.left);
+        System.out.print(node.value);
+        inOrder(node.right);
+    }
+
+    public void postOrder(char value) {
+        if(value == '.') {
+            return;
+        }
+
+        Node node = tree[index(value)];
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value);
+    }
+
+    private int index(char value) {
+        return value - 'A';
+    }
+
+    class Node {
+        private char value;
+
+        private char left;
+        private char right;
+
+        public Node(char value, char left, char right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
