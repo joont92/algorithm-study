d3ee56a8758fd88135e4d9c537282b4e8497c478
joont92 <pk920207@gmail.com>
2020-11-19 23:03:40 +0900

    요세푸스 문제

--- src/main/java/baekjoon/datastructure/요세푸스_문제.java
+++ src/main/java/baekjoon/datastructure/요세푸스_문제.java
@@ -1,10 +1,12 @@
 package baekjoon.datastructure;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
@@ -14,16 +16,25 @@ import java.util.stream.IntStream;
  * 제거하고자 하는 순서가 되었을 때 큐의 마지막에 넣어주지 않고 제거하면 된다
  */
 public class 요세푸스_문제 {
-    public List<Integer> removedOrder(int n, int k) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int n = Integer.parseInt(str[0]);
+        int k = Integer.parseInt(str[1]);
+
+        요세푸스_문제 fn = new 요세푸스_문제();
+        fn.solution(n, k).forEach(System.out::println);
+    }
+
+    private List<Integer> solution(int n, int k) {
         List<Integer> result = new ArrayList<>();
 
-        Queue<Integer> queue = IntStream.rangeClosed(1, n)
-                .boxed()
-                .collect(Collectors.toCollection(LinkedList::new));
+        Queue<Integer> queue = new LinkedList<>();
+        IntStream.rangeClosed(1, n).forEach(queue::offer);
 
-        while (queue.size() > 0) {
+        while (!queue.isEmpty()) {
             for (int i = 0; i < k - 1; i++) {
-                queue.add(queue.poll());
+                queue.offer(queue.poll());
             }
             result.add(queue.poll());
         }
